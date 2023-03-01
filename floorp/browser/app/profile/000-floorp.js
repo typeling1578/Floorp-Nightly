a701df77af528a9d2f69ef6232af746edced6b6b
Ryosuke Asano <ryosukeusuusu@gmail.com>
2023-03-01 23:27:56 +0900

    メモ機能の同期を有効にします

--- floorp/browser/app/profile/000-floorp.js
+++ floorp/browser/app/profile/000-floorp.js
@@ -111,6 +111,7 @@ pref("floorp.display.toolbarbutton.label", true);
 // 1つ目はタイトル、2つ目は内容
 pref("floorp.browser.note.memos", "");
 pref("floorp.browser.note.memos.using", -1);
+pref("services.sync.prefs.sync.floorp.browser.note.memos", true);
 
 /*----------------------------------------------------------------------------------------------------------------------------------*/
 

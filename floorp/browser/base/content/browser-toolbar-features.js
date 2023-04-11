513a970594d5873b8bdaf78a8b7b6e4cb8b14b7d
regularenthropy <roottotalhax@gmail.com>
2023-04-11 20:00:29 +0200

    [FB] Widgets | Improve clock widget

--- floorp/browser/base/content/browser-toolbar-features.js
+++ floorp/browser/base/content/browser-toolbar-features.js
@@ -3,28 +3,35 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let { CustomizableUI } = ChromeUtils.import(
-  "resource:///modules/CustomizableUI.jsm"
-);
-let { SessionStore } = ChromeUtils.import("resource:///modules/sessionstore/SessionStore.jsm")
-async function UCTFirst() {
-  let l10n = new Localization(["browser/floorp.ftl"])
-  let l10n_text = await l10n.formatValue("undo-closed-tab") ?? "Undo close Tab"
+const { CustomizableUI } = ChromeUtils.import("resource:///modules/CustomizableUI.jsm");
+const { SessionStore } = ChromeUtils.import("resource:///modules/sessionstore/SessionStore.jsm")
+
+async function UCTFirst(){
+  const l10n = new Localization(["browser/floorp.ftl"])
+  const l10n_text = await l10n.formatValue("undo-closed-tab") ?? "Undo close tab"
   CustomizableUI.createWidget({
     id: 'undo-closed-tab',
     type: 'button',
     label: l10n_text,
     tooltiptext: l10n_text,
     onCreated(aNode) {
-      aNode.appendChild(window.MozXULElement.parseXULToFragment(`<stack xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" class="toolbarbutton-badge-stack"><image class="toolbarbutton-icon" label="閉じたタブを開く"/><html:label xmlns:html="http://www.w3.org/1999/xhtml" class="toolbarbutton-badge" ></html:label></stack>`))
+        aNode.appendChild(
+          window.MozXULElement.parseXULToFragment(
+            `<stack xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" class="toolbarbutton-badge-stack"><image class="toolbarbutton-icon" label="閉じたタブを開く"/><html:label xmlns:html="http://www.w3.org/1999/xhtml" class="toolbarbutton-badge" ></html:label></stack>`
+            )
+          )
 
-      let a = aNode.querySelector(".toolbarbutton-icon:not(stack > .toolbarbutton-icon)")
-      if (a != null) a.remove()
+        let a = aNode.querySelector(".toolbarbutton-icon:not(stack > .toolbarbutton-icon)")
+        if (a != null) {
+          a.remove()
+        }
     },
     onCommand() {
-      let workingWindow = Services.wm.getMostRecentWindow("navigator:browser")
-      if (SessionStore.getClosedTabCount(workingWindow)) SessionStore.undoCloseTab(workingWindow)
-    }
+        let workingWindow = Services.wm.getMostRecentWindow("navigator:browser")
+        if (SessionStore.getClosedTabCount(workingWindow)){
+          SessionStore.undoCloseTab(workingWindow)
+        }
+      }
   });
   if (ChromeUtils.import("resource:///modules/BrowserManagerSidebar.jsm").isFirstRun) {
     CustomizableUI.addWidgetToArea("undo-closed-tab", CustomizableUI.AREA_NAVBAR, -1);
@@ -58,8 +65,8 @@ switchSidebarPositionButton()
 /**************************************** clock ****************************************/
 
 
-let locale = Cc["@mozilla.org/intl/ospreferences;1"].getService(Ci.mozIOSPreferences).regionalPrefsLocales;
-const options = { month: 'short', day: 'numeric', weekday: 'short' }
+const locale = Cc["@mozilla.org/intl/ospreferences;1"].getService(Ci.mozIOSPreferences).regionalPrefsLocales;
+const options = { month: 'short', day: 'numeric', weekday: 'short'}
 
 function ClockFirst() {
   let vanilla = "--:--"
@@ -72,9 +79,8 @@ function ClockFirst() {
 }
 
 function checkBrowserLangForLabel() {
-  let now = new Date();
-
-  if (locale[0] == "ja-JP") {
+  const now = new Date();
+  if(locale[0] == "ja-JP"){
     return now.toLocaleDateString('ja-JP', options);
   } else {
     return now.toLocaleDateString();
@@ -82,32 +88,29 @@ function checkBrowserLangForLabel() {
 }
 
 function checkBrowserLangForToolTipText() {
-  let now = new Date();
-  let year = now.getFullYear();
-  let JPYear = year - 2018;
-
-  if (locale[0] == "ja-JP") {
-    return `${year}年` + `(令和${JPYear}年) ` + now.toLocaleDateString('ja-JP', options);
+  const now = new Date();
+  const year = now.getFullYear();
+  const JPYear = year - 2018;
+  if(locale[0] == "ja-JP"){
+    return `${year}年`+`(令和${JPYear}年) `+ now.toLocaleDateString('ja-JP', options);
   } else {
     return now.toLocaleDateString();
   }
 }
 
 function setNowTime() {
-  let now = new Date();
-
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
-  if (hours < 10) hours = "0" + hours;
-  if (minutes < 10) minutes = "0" + minutes;
-  let dateAndTime = `${checkBrowserLangForLabel()} ${hours}:${minutes}`;
-  let withSeconds = `${checkBrowserLangForToolTipText()} ${hours}:${minutes}:${seconds}`;
+  const now = new Date();
+  const timeFormat = new Intl.DateTimeFormat(navigator.language, {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+  let timeString = timeFormat.format(now);
   let clock = document.getElementById("toolbarItemClock");
-  clock.setAttribute("label", dateAndTime);
-  clock.setAttribute("tooltiptext", withSeconds);
+  clock.setAttribute("label", checkBrowserLangForLabel() + " " + timeString);
+  clock.setAttribute("tooltiptext", checkBrowserLangForToolTipText() + " " + timeString);
 }
 
-if (Services.prefs.getBoolPref("floorp.browser.clock.enabled")) {
-  ClockFirst();
+if(Services.prefs.getBoolPref("floorp.browser.clock.enabled")) {
+  ClockFirst();  
 }
